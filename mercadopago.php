5bd06e04fd3108a441038259fc3fa8e14e44dc1f
MatiasY <matiasyacante@yahoo.com.ar>
2021-05-31 16:07:59 -0300

    prueba de respuesta

--- mercadopago.php
+++ mercadopago.php
@@ -8,7 +8,7 @@ $dominio = "http://".$dominio;
 // Credenciales
 // Public Key: TEST-667354d7-fc8d-47c6-82b0-b315e448b6d6
 // Access Token: TEST-1206668190429489-112115-bcb9f9fcb87c419c6fbe5a6e0144bcc4__LD_LB__-53342228
-MercadoPago\SDK::setAccessToken("TEST-1206668190429489-112115-bcb9f9fcb87c419c6fbe5a6e0144bcc4__LD_LB__-53342228"); // Either Production or SandBox AccessToken
+MercadoPago\SDK::setAccessToken("APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439"); // Either Production or SandBox AccessToken
 
 // Crea un objeto de pago
 $payment = new MercadoPago\Payment();
@@ -44,9 +44,11 @@ $preference->save();
 // b) Número de la casa: 1602
 // c) Código postal: 03940
 $payer = new MercadoPago\Payer();
+$payer->id = "681051270";
 $payer->name = "Lalo";
 $payer->surname = "Landa";
 $payer->email = "test_user_46542185@testuser.com";
+$payer->password = "qatest9922";
 $payer->date_created = "2018-06-02T12:58:41.425-04:00";
 $payer->phone = array(
     "area_code" => "",
@@ -61,6 +63,19 @@ $payer->address = array(
     "street_number" => 1602,
     "zip_code" => "03940"
 );
+
+
+// Excluir metodos de pago
+$preference->payment_methods = array(
+  "excluded_payment_methods" => array(
+    array("id" => "diners")
+  ),
+  "excluded_payment_types" => array(
+    array("id" => "atm")
+  ),
+  "installments" => 6
+);
+// ...
 
 
 
